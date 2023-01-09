c8a86c927cd313161319dcde73fcf5ccbdd93ad4
Gustavo <90078770+Gustavo3236@users.noreply.github.com>
2023-01-09 19:59:56 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,4 +3,15 @@ let botaoMenu = document.querySelector(".botao__menu");
 
 botaoMenu.addEventListener("click", () => {
     menu.classList.toggle("menu-lateral--ativo");
-})
+});
+
+$('.carrossel').slick({
+    arrows: false,
+    dots: true,
+    infinite: true,
+    speed: 1500,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    centerMode: true,
+  });
